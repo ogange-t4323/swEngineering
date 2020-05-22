93e6e68103706b14ab7e3296140e555af6fed64d
tonny iro <tonnyogange@gmail.com>
2020-05-22 01:07:04 -0700

    commiting everything

---
+++ src/maJavaProject/myAppTester.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package maJavaProject;
+
+import java.util.Scanner;
+
+/**
+ * @author tonny
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello there! Welcome to my application");
+		System.out.println("Enter a number between 1 and 5 to know what animal you would be");
+		Scanner input  = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		if(n==1) {
+			System.out.println("you are a lion");
+		}else if(n==2) {
+			System.out.println("you are a rabbit");
+		}else if(n==3) {
+			System.out.println("you are a frog");
+
+		}else if(n==4) {
+			System.out.println("you are a lizard");
+			
+		}else if(n==5) {
+			System.out.println("you are not an animal");
+			
+		}else{
+			System.out.println("Invalid Entry!");
+			
+		}
+		
+	}
+
+}
